a6f1b5c7237987c2dffdfdd0d2a25faa6cd04a81
MLD <andreimladenovici@yahoo.ro>
2021-05-13 15:30:55 +0300

    Added Menu and Splash Screen.

---
+++ app/src/main/java/com/hfad/fcbarcelonaplayerdatabase/Menu.java
@@ -0,0 +1,50 @@
+package com.hfad.fcbarcelonaplayerdatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Menu extends AppCompatActivity {
+
+    Button btn_Start, btn_MH;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+
+        btn_Start = findViewById(R.id.btn_Start);
+        btn_MH = findViewById(R.id.btn_MH);
+
+
+        btn_Start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent(Menu.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+
+        });
+
+        btn_MH.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent(Menu.this, MatchHistory.class);
+                startActivity(intent);
+            }
+
+
+        });
+
+    }
+
+}
